8d63de5e2f17282ce4cb3e4758d1aac1a7594469
Dereje-get <derget17@gmail.com>
2025-06-26 08:46:32 -0400

    css added

--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -1,33 +1,42 @@
-import React, { useContext } from 'react'
-import Rating from '@mui/material/Rating'
-import CurrencyFormat from '../CurrencyFormat/CurrencyFormat';
-import classes from './product.module.css';
-import { Link } from 'react-router-dom';
-import { DataContext } from '../DataProvider/DataProvider';
-import { Type } from '../../Utility/action.type';
+import React, { useContext } from "react";
+import Rating from "@mui/material/Rating";
+import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
+import classes from "./Product.module.css";
+import { Link } from "react-router-dom";
+import { DataContext } from "../DataProvider/DataProvider";
+import { Type } from "../../Utility/action.type";
 
-function ProductCard({product, flex, renderDesc, renderAdd}) {
-  const { image, title, id, rating, price, description} = product;
+function ProductCard({ product, flex, renderDesc, renderAdd }) {
+  const { image, title, id, rating, price, description } = product;
 
-  const [state, dispatch] = useContext(DataContext)
-  
+  const [state, dispatch] = useContext(DataContext);
+
   const addToCart = () => {
     dispatch({
       type: Type.ADD_TO_BASKET,
       item: {
-        image, title, id, rating, price, description
-      }
+        image,
+        title,
+        id,
+        rating,
+        price,
+        description,
+      },
     });
-  }
+  };
 
   return (
-    <div className={`${classes.card__container} ${flex?classes.product__flexed : ''}`}>
+    <div
+      className={`${classes.card__container} ${
+        flex ? classes.product__flexed : ""
+      }`}
+    >
       <Link to={`/products/${id}`}>
         <img src={image} alt="image" className={classes.img_container} />
       </Link>
       <div>
         <h3>{title}</h3>
-        {renderDesc && <div style={{maxWidth:"750px"}}>{description}</div>}
+        {renderDesc && <div style={{ maxWidth: "750px" }}>{description}</div>}
         <div className={classes.rating}>
           {/* rating */}
           <Rating value={rating?.rate} precision={0.1} />
@@ -39,12 +48,14 @@ function ProductCard({product, flex, renderDesc, renderAdd}) {
           <CurrencyFormat amount={price} />
         </div>
       </div>
-      {
-        renderAdd && <button className={classes.button} onClick={addToCart}> add to cart</button>
-      }
-      
+      {renderAdd && (
+        <button className={classes.button} onClick={addToCart}>
+          {" "}
+          add to cart
+        </button>
+      )}
     </div>
   );
 }
 
-export default ProductCard
+export default ProductCard;
