7508c42e5d884c15599966cb69798815ae4254e4
Dereje-get <derget17@gmail.com>
2025-06-26 08:27:21 -0400

    module css is added

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
+import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import { img }  from './Img/Data'
+import { img } from "./Img/Data";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import classes from '../Carousel/carousel.module.css';
+import classes from "./Carousel.module.css";
 
 function CarouselEffect() {
   return (
     <div>
-        <Carousel
-            autoPlay={true}
-            infiniteLoop={true}
-            showIndicators={false}
-            showThumbs={false}
-        >
-        
-        {
-            img.map((imageItemLink) => {
-                return <img src={imageItemLink}/>
-            })
-        }
-          </Carousel>
-        <div className={classes.hero__img}></div>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+      >
+        {img.map((imageItemLink) => {
+          return <img src={imageItemLink} />;
+        })}
+      </Carousel>
+      <div className={classes.hero__img}></div>
     </div>
-  )
+  );
 }
 
-export default CarouselEffect
+export default CarouselEffect;
