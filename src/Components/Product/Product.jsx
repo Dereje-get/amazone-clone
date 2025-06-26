f57bded3313130e9d09543ebdd31be9c4b96621f
Dereje-get <derget17@gmail.com>
2025-06-26 08:40:37 -0400

    css added to product

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,39 +1,47 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import ProductCard from './ProductCard';
-import classes from './Product.module.css';
-import Loader from '../../Components/Loader/Loader';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "./ProductCard";
+import classes from "./Product.module.css";
+import Loader from "../../Components/Loader/Loader";
 
 function Product() {
-    const [products, setProducts] = useState([]);
-    const [isLoading, setLoading]   = useState(true)
+  const [products, setProducts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setLoading(true)
-        axios.get("https://fakestoreapi.com/products").then((res) => {
-            setProducts(res.data);
-            setLoading(false)
-            console.log(res.data);
-            }).catch((error)=>{
-                console.log(error);
-                setLoading(false)
-            })
-    }, [])
-    return (
-        <>
-            {
-                isLoading? (<Loader/>): (<section className={classes.products__container}>
-                {products?.map((singleProduct) => (
-                <ProductCard renderAdd={true} product={singleProduct} key={singleProduct.id} />
-                ))}
-            </section>)
-            }
-            
-        </>
-    );
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className={classes.products__container}>
+          {products?.map((singleProduct) => (
+            <ProductCard
+              renderAdd={true}
+              product={singleProduct}
+              key={singleProduct.id}
+            />
+          ))}
+        </section>
+      )}
+    </>
+  );
 }
 
-export default Product
+export default Product;
 
 // function Product() {
 //   const [products, setProducts] = useState([]); //initialize products as an empty array to ensure .map() works on the first render, as .map() works on empty arrays — it just returns an empty array.
