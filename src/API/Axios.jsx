a7ec6bad10a91f3c65c73812707227fde3cb3c01
Dereje-get <derget17@gmail.com>
2025-06-27 09:14:27 -0400

    api edited

--- src/API/Axios.jsx
+++ src/API/Axios.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // local instance of fire base functions
-//   baseURL: "http://127.0.0.1:5001/clone-93b3c/us-central1/api",
+  //   baseURL: "http://127.0.0.1:5001/clone-93b3c/us-central1/api",
   // deployed version of firebase function
-    baseURL: "https://api-qabhq7ydtq-uc.a.run.app/",
+  baseURL: "https://us-central1-clone-93b3c.cloudfunctions.net/api",
   // deployed virsion of amazon server on render.com
   // baseURL: "https://amazon-api-76qq.onrender.com/",
 });
